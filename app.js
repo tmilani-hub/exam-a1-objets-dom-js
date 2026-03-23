427b733ac0d607a066bba06019b63e6841ffc37b
tmilani-hub <tmilani@edenschool.fr>
2026-03-23 12:22:58 +0100

    finish

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const citations = [
 	{
-		Sitations: "Un esprit curieux est un esprit sans limites.",
-		Auteur: "Neil deGrasse Tyson",
+		Sitation: "Un esprit curieux est un esprit sans limites.",
+		Auteur: "Neil de Grasse Tyson",
 		Date: 1792,
 		Couleur_de_fond: "#44e5ca",
 		Couleur_du_bouton: "#4c0f19",
@@ -46,7 +46,12 @@ const $body = document.querySelector("body");
 const $h1 = document.querySelector("h1");
 const $h5 = document.querySelector("h5");
 const $p = document.querySelector("p");
+const $span = document.querySelector("span");
 const $btn = document.querySelector("button");
+
+document.addEventListener("DOMContentLoaded", () => {
+	refresh();
+});
 
 $btn.addEventListener("click", () => {
 	refresh();
@@ -54,4 +59,13 @@ $btn.addEventListener("click", () => {
 
 function refresh() {
 	let object = citations[Math.floor(Math.random() * citations.length)];
+	$body.style.backgroundColor = object.Couleur_de_fond;
+	$h1.textContent = "'" + object.Sitation + "'";
+	$h5.textContent = object.Auteur;
+	$p.textContent = object.Origine;
+	$span.textContent = ", " + object.Date;
+	$btn.style.backgroundColor = object.Couleur_du_bouton;
+	return object;
 }
+
+setInterval(refresh, 10000);
